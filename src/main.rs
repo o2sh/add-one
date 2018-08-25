b4a44d05a6edf74617b409ce188fcd3aecea08f1
Ossama Hjaji <ossama-hjaji@live.fr>
2018-08-25 11:28:00 +0200

    Integer overflow: i32 to BigInt

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use std::env;
+extern crate num_bigint;
+extern crate num_traits;
 
-use std::num::ParseIntError;
+use num_bigint::{BigInt, ParseBigIntError};
+use num_traits::FromPrimitive;
+use std::env;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -14,11 +17,14 @@ fn main() {
     }
 }
 
-fn add_one(number_str: &str) -> Result<i32, ParseIntError> {
-    number_str.parse::<i32>().map(|n| n + 1)
+fn add_one(number_str: &str) -> Result<BigInt, ParseBigIntError> {
+    match number_str.parse::<BigInt>() {
+        Ok(n) => Ok(n + 1),
+        Err(err) => Err(err),
+    }
 }
 
 #[test]
 fn it_works() {
-    assert_eq!(add_one("2").unwrap(), 3)
+    assert_eq!(add_one("2").unwrap(),BigInt::from_u64(3).unwrap())
 }
