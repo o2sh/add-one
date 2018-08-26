c49161c18bdd50a5f42229935a87a4dcfb7a1310
Mara Bos <m-ou.se@m-ou.se>
2018-08-26 10:23:55 +0200

    Don't use bigint, but increment decimal number directly.
    This is both faster and more memory efficient.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-extern crate num_bigint;
-extern crate num_traits;
 extern crate add_one;
 
-use std::env;
 use add_one::add_one;
+use std::env;
+use std::io::stdout;
 
 fn main() {
     let mut args = env::args();
@@ -25,29 +24,8 @@ fn main() {
         return;
     };
 
-    let output_str = add_one(&input_str);
-
-    match output_str {
-        Ok(v) => println!("{}", v),
-        Err(e) => eprintln!("Bad Input: {}", e),
+    match add_one(input_str.as_bytes(), &mut stdout()) {
+        Ok(()) => println!(),
+        Err(e) => eprintln!("Error: {}", e),
     }
 }
-
-#[cfg(test)]
-mod tests {
-    use num_traits::FromPrimitive; 
-    use num_bigint::BigInt;
-    use super::*; 
-    #[test] 
-    fn it_works() {
-        assert_eq!(add_one("2").unwrap(), BigInt::from_i64(3).unwrap());
-        assert_eq!(add_one("-11").unwrap(), BigInt::from_i64(-10).unwrap());
-        assert_eq!(
-            add_one("1256146513513224524524524524522452165841613615616516516").unwrap(),
-            "1256146513513224524524524524522452165841613615616516517"
-                .parse::<BigInt>()
-                .unwrap()
-        );
-    }
-}
-
